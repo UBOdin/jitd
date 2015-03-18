f8e7340d18b7d68d9f19f90ed590428f3b405877
Oliver Kennedy <okennedy@buffalo.edu>
2015-03-18 17:28:22 -0400

    Workload is now bigger

--- cpp/src/workload.cpp
+++ cpp/src/workload.cpp
@@ -4,37 +4,63 @@
 
 using namespace std;
 
+
 int main(int argc, char** argv)
 {
-  int high_mark = 1000*1000*10;
-  int low_mark = 1000*1000*1;
-  int batch_size = 1000*10;
-  int op_count = 1000;
+  long int high_mark = 1000*1000*1000*10;
+  long int low_mark = 1000*1000*1000*1;
+  int batch_size = 1000*1000*100;
+  long int op_count = 100;
   int max_key = 1000*1000*100;
-  vector<int> values;
+  vector< vector<int> > values;
   
-  int diff_mark = high_mark - low_mark;
-  int op;
+  long int diff_mark = high_mark - low_mark;
+  long int op;
+  
+  srand(time(NULL));
   
   for(op = 0; op < op_count; op++)
   {
-    if(op % 100 == 0){ cerr << "." << op; cerr.flush(); }
-    if((rand() % diff_mark) > values.size() - low_mark){
+//    if(op % 10 == 0){ 
+      cerr <<endl<< ":" << op; cerr.flush(); 
+//      }
+    long int choice = rand() % diff_mark;
+    long int threshold = values.size() - low_mark;
+    char cmd;
+    int cnt;
+    if(choice > threshold){
       int i;
-      cout << "insert explicit";
-      for(i = 0; i < batch_size; i++){
+      cmd = 'i';
+      cnt = batch_size;
+      cout.write((char *)&cmd, sizeof(cmd));
+      cout.write((char *)&cnt, sizeof(cnt));
+      
+      values.emplace_back(cnt);
+      
+      for(i = 0; i < cnt; i++){
+        if(i % (1000*1000) == 0){
+          cerr << "." << i; cerr.flush();
+        }
         int k = rand() % max_key;
-        values.push_back(k);
-        cout << " " << k;
+        values.back().push_back(k);
+        cout.write((char *)&k, sizeof(k));
       }
+      random_shuffle(values.back().begin(), values.back().end());
       cout << endl;
     } else {
       int i;
-      cout << "remove explicit";
-      random_shuffle(values.begin(), values.end());
-      for(i = 0; i < batch_size; i++){
-        cout << " " << values.back();
-        values.pop_back();
+      cmd = 'r';
+      cnt = batch_size;
+      cout.write((char *)&cmd, sizeof(cmd));
+      cout.write((char *)&cnt, sizeof(cnt));
+      for(i = 0; i < cnt; i++){
+        if(i % (1000*1000) == 0){
+          cerr << ":" << i; cerr.flush();
+        }
+        int id = rand() % values.size();
+        int k = values[id].back();
+        cout.write((char *)&k, sizeof(k));
+        values[id].pop_back();
       }
       cout << endl;
     }
