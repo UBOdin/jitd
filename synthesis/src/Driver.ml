f6f194c5947659fc0ff8fe383af1f068ae9fa684
Oliver Kennedy <okennedy@buffalo.edu>
2015-03-30 16:51:51 -0400

    Adding handle support to the codegen

--- synthesis/src/Driver.ml
+++ synthesis/src/Driver.ml
@@ -18,6 +18,9 @@ let arg_spec = Arg.align [
      | "cpp"  -> output_format := FORMAT_CPP
      | _      -> raise Not_found
     ), "Set the output format"
+  );
+  ( "-j", Arg.Unit(function () ->  output_format := FORMAT_JITDSL
+    ), "JITD debugging mode"
   )
 ];;
 
@@ -27,23 +30,30 @@ Arg.parse
 	"jitd [options] files";;
 
 let prog = 
-  (List.fold_left merge_programs empty_program
-    (List.map 
-      (fun f -> 
-        let buf = (Lexing.from_channel (open_in f)) in
-          try 
-            JITDParser.program JITDLexer.token buf
-          with 
-            | JITD.ParseError(msg, pos) ->
-                print_endline ("Syntax Error ("^
-                                f^":"^
-                                (string_of_int pos.Lexing.pos_lnum)^" '"^
-                                (Lexing.lexeme buf)^"'): "^msg);
-                exit (-1)
+  try
+    (List.fold_left merge_programs empty_program
+      (List.map 
+        (fun f -> 
+          let buf = (Lexing.from_channel (open_in f)) in
+            try 
+              JITDParser.program JITDLexer.token buf
+            with 
+              | JITD.ParseError(msg, pos) ->
+                  print_endline ("Syntax Error ("^
+                                  f^":"^
+                                  (string_of_int pos.Lexing.pos_lnum)^" '"^
+                                  (Lexing.lexeme buf)^"'): "^msg);
+                  exit (-1)
+        )
+        (List.rev !source_files) 
       )
-      (List.rev !source_files) 
     )
-  )
+  with
+    | Sys_error(msg) -> (
+      print_endline ("Error: "^msg);
+      exit (-1)
+    )
+      
 ;;
 
 
